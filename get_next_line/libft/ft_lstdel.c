187b854fd460737b990fd7ba3c637f1822b02b47
Emeric <edescoin@42.student.fr>
2017-01-02 16:02:24 +0100

    ajout des anciens fichiers

---
+++ get_next_line/libft/ft_lstdel.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 14:31:18 by edescoin          #+#    #+#             */
+/*   Updated: 2016/11/22 16:46:05 by edescoin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	if (alst && *alst && del)
+	{
+		if ((*alst)->next)
+			ft_lstdel(&(*alst)->next, del);
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+}
