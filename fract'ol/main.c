9a1a9e28490826e02f8fd4e5051c08d5d9df02ab
Quantum Soul <emericdescoings@yahoo.fr>
2017-02-10 19:26:28 +0100

    tests (concluants) sur un début d'affichages des fractales et sur mandelbrot

--- fract'ol/main.c
+++ fract'ol/main.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/03 16:11:38 by edescoin          #+#    #+#             */
-/*   Updated: 2017/02/10 19:10:39 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/02/10 19:26:24 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <fcntl.h>
 #include <math.h>
 
-void	exit_error(char *s)
+void		exit_error(char *s)
 {
 	if (!s)
 	{
@@ -26,14 +26,14 @@ void	exit_error(char *s)
 	exit(1);
 }
 
-void	exit_main(void)
+void		exit_main(void)
 {
 	mlx_destroy_window(mlx_get_core(0,0)->co, mlx_get_core(0,0)->win);
 	garbage_collector(CLEAR, NULL, NULL);
 	exit(0);
 }
 
-void	create_events(t_key_evt **head, t_mlx_core *core)
+void		create_events(t_key_evt **head, t_mlx_core *core)
 {
 /*	mlx_do_key_autorepeaton(core->co);
 	new_key_evt(head, K_Z, &translate_up);
@@ -44,7 +44,18 @@ void	create_events(t_key_evt **head, t_mlx_core *core)
 	mlx_hook(core->win, K_PRESS_EVT, K_PRESS_MASK, &key_hook, *head);
 }
 
-int		main(int ac, char **av)
+t_fractal	**init_ftl_tab()
+{
+	t_fractal	**tab;
+
+	if (!(tab = malloc(sizeof(t_fractal*) + 1)))
+		return (NULL);
+	tab[1] = create_mandelbrot();
+	tab[2] = NULL;
+	return (tab);
+}
+
+int			main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
@@ -52,18 +63,18 @@ int		main(int ac, char **av)
 	t_key_evt	*events;
 	int			width;
 	int			height;
-	t_fractal	*ftl;
+	t_fractal	**ftl_tab;
 	t_image		*img;
 
-	ftl = create_mandelbrot();
-	height = (ftl->y_max - ftl->y_min) * ftl->zoom;
-	width = (ftl->x_max - ftl->x_min) * ftl->zoom;
+	ftl_tab = init_ftl_tab();
+	height = ((*ftl_tab)->y_max - (*ftl_tab)->y_min) * (*ftl_tab)->zoom;
+	width = ((*ftl_tab)->x_max - (*ftl_tab)->x_min) * (*ftl_tab)->zoom;
 	core = mlx_get_core(width, height);
 	img = create_image(width, height, 32);
 	events = init_key_evts(K_ECHAP, &exit_main);
 	create_events(&events, core);
 
-	draw_fractal(img, ftl);
+	draw_fractal(img, *ftl_tab);
 
 	mlx_loop(core->co);
 	exit_main();
