f805130a14e48d823ee9b137226ce708e9374cd9
Quantum Soul <emericdescoings@yahoo.fr>
2017-08-11 17:11:31 +0200

    ajout du chgmt de résolution avec touche haut et bas

--- rtv1/src/key_functions.c
+++ rtv1/src/key_functions.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 14:48:19 by edescoin          #+#    #+#             */
-/*   Updated: 2017/08/11 15:39:02 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/08/11 17:08:33 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,18 @@ int	key_management(SDL_Event *current, t_event *evt)
 	else if (current->key.keysym.sym == SDLK_F5)
 	{
 		data->scene->cam->resolution = 1;
+		init_cam_screen(data->scene->cam);
+		render_scene(data->scene);
+	}
+	else if (current->key.keysym.sym == SDLK_UP ||
+			current->key.keysym.sym == SDLK_DOWN)
+	{
+		if (current->key.keysym.sym == SDLK_UP &&
+			data->scene->cam->low_resolution > 1)
+			data->scene->cam->low_resolution--;
+		else if (current->key.keysym.sym == SDLK_DOWN)
+			data->scene->cam->low_resolution++;
+		data->scene->cam->resolution = data->scene->cam->low_resolution;
 		init_cam_screen(data->scene->cam);
 		render_scene(data->scene);
 	}
